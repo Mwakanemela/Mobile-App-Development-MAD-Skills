de71ac6b107592f8ab514bf5a2b1c67eb3e70905
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2024-02-14 22:07:47 +0200

    need more practical knowledge about lifecycle owner and lifecycle observer - nice concept though

--- LifecycleDemo/app/src/main/java/com/example/lifecycledemo/MainActivity.kt
+++ LifecycleDemo/app/src/main/java/com/example/lifecycledemo/MainActivity.kt
@@ -11,7 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        timerToast = TimerToast(application)
-        timerToast.startTimer()
+        timerToast = TimerToast(application, this)
     }
 }
--- LifecycleDemo/app/src/main/java/com/example/lifecycledemo/TimerToast.kt
+++ LifecycleDemo/app/src/main/java/com/example/lifecycledemo/TimerToast.kt
@@ -3,23 +3,43 @@ package com.example.lifecycledemo
 import android.app.Application
 import android.os.CountDownTimer
 import android.widget.Toast
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.OnLifecycleEvent
 
-class TimerToast(application: Application) {
+class TimerToast(application: Application, lifecycleOwner: LifecycleOwner) : LifecycleObserver {
+
+    private var started = false
+    private val lifecycle = lifecycleOwner.lifecycle
+
+    init {
+        lifecycle.addObserver(this)
+    }
     //for each 60s the timer will run and get a toast for each 3s interval
-    private val timer: CountDownTimer = object: CountDownTimer(6000, 3000) {
+    private val timer: CountDownTimer = object: CountDownTimer(60000, 3000) {
         override fun onTick(milliSecondsUntilFinished: Long) {
-            Toast.makeText(application, "$milliSecondsUntilFinished", Toast.LENGTH_SHORT).show()
+            if(lifecycle.currentState.isAtLeast(Lifecycle.State.STARTED)) {
+                Toast.makeText(application, "$milliSecondsUntilFinished", Toast.LENGTH_SHORT).show()
+            }
         }
 
         override fun onFinish() {
-            Toast.makeText(application, "Finished", Toast.LENGTH_SHORT).show()
+            //run toast on ui thread
+            if(lifecycle.currentState.isAtLeast(Lifecycle.State.STARTED)) {
+                Toast.makeText(application, "Finished", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun startTimer() {
-        timer.start()
+        if(!started) {
+            started = true
+            timer.start()
+        }
     }
-
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     fun stopTimer() {
         timer.cancel()
     }
