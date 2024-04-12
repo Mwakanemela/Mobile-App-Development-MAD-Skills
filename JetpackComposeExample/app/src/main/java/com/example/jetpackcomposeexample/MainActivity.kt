8c92e7a878296c81db8ba7a4ef8571408d23a57d
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2024-04-12 03:26:41 +0200

    Surface in jetpack compose

--- JetpackComposeExample/app/src/main/java/com/example/jetpackcomposeexample/MainActivity.kt
+++ JetpackComposeExample/app/src/main/java/com/example/jetpackcomposeexample/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.jetpackcomposeexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposeexample.ui.theme.JetpackComposeExampleTheme
 
@@ -24,8 +31,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun FirstApp() {
-    Surface(modifier = Modifier.fillMaxSize()) {
-        Text(text = "Mwakanemela Kayange", fontSize = 30.sp)
+    Surface(
+        modifier = Modifier.height(150.dp).width(250.dp).padding(20.dp),
+        shape = CutCornerShape(8.dp),
+        color = Color.Gray,
+        border = BorderStroke(2.dp, Color.Green),
+        shadowElevation = 20.dp
+        ) {
+        Text(text = "Mwakanemela Kayange", fontSize = 20.sp, modifier = Modifier.padding(8.dp))
     }
 }
 
